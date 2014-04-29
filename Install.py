fdcbbcec02912064d2308e8fb6848e8fcaf9d950
Restioson <cael.marquard@gmail.com>
2014-04-29 09:57:11 +0200

    Improved Install.py

--- Install.py
+++ Install.py
@@ -1,6 +1,9 @@
-import os
-source = './api/python/mcpi/'
-destination = './_mods/'
-flag = '-r'
-command = 'cp'
-os.system( command+' '+flag+' '+' '+source+' '+destination )
+#Install the dependencies for MinecraftOven
+import os #Import modules
+print('If errors were thrown, try running this program as root or run "sudo Install.py"')
+print('Copying the API from ~/mcpi/api/python/mcpi/ to ~/mcpi/_mods/mcpi/')
+os.system('cp -r ./api/python/mcpi/ ./_mods/') #Copy the API
+print('The API requires pygame to run.')
+print('Installing pygame...')
+os.system('apt-get install python-pygame') #Install dependencies
+print('Install complete')
