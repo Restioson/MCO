e000c6ffc0773bdaf64584302f5f316734f19ce1
Restioson <cael.marquard@gmail.com>
2014-04-29 12:12:36 +0200

    Fatal Bug in Grill

--- _mods/Grill_Builtins.py
+++ _mods/Grill_Builtins.py
@@ -10,6 +10,6 @@ mc = minecraft.Minecraft.create() #Connect to minecraft
 api.general.msg('GrillTools has been successfully enabled', mc)
 while True:
 	hit = api.event.getHit(mc)
-	api.general.msg('You clicked at '+str(hit[0])+', '+str(hit[1])+', '+str(hit[2])+' on a/n 'str(dir(block.Block(hit[4])))+' Block')
+	api.general.msg('You clicked at '+str(hit[0])+', '+str(hit[1])+', '+str(hit[2])+' on a/n '+str(dir(block.Block(hit[4])))+' Block')
 	print('[Info] Grill_Builtins has ended...')#Tell the user that the program has ended
 #EndOfMod
